781df9347986e7f6e4b6e063d3deb49660d1270f
Kevin <351916362@qq.com>
2015-07-02 10:51:28 +0800

    创建Server类，用于做TCP的服务端

---
+++ src/com/oracle/oaec/Server.java
@@ -0,0 +1,17 @@
+package com.oracle.oaec;
+
+public class Server {
+
+	/**
+	 * @功能描述: 请用一句话描述这个方法的功能
+	 * @作者: Kevin
+	 * @时间: 2015-7-2上午10:50:32
+	 * @版本: V1.0.0.0 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
